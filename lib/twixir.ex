d95439056a629ed177cdb33820a3f86aeaa78107
JCGrant <jamescolin.grant@gmail.com>
2017-11-18 19:29:37 +0000

    Initialise Elixir project

---
+++ lib/twixir.ex
@@ -0,0 +1,18 @@
+defmodule Twixir do
+  @moduledoc """
+  Documentation for Twixir.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Twixir.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/twixir_test.exs
@@ -0,0 +1,8 @@
+defmodule TwixirTest do
+  use ExUnit.Case
+  doctest Twixir
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
